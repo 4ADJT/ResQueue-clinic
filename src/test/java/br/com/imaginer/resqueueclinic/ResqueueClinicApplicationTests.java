207d2a48545beedcda5d7067d8694704ce601c3f
rodrigobrocchi <rodrigo.brocchi@gmail.com>
2025-02-03 20:12:02 -0300

    first commit

---
+++ src/test/java/br/com/imaginer/resqueueclinic/ResqueueClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.imaginer.resqueueclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResqueueClinicApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
