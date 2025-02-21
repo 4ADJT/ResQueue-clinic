f08a3896f99d615bf848fe620789bade32705d26
rony1533 <ronysobral5@gmail.com>
2025-02-21 20:17:47 -0300

    refactor: alterando o tipo de classe para record

--- src/main/java/br/com/imaginer/resqueueclinic/domain/model/User.java
+++ src/main/java/br/com/imaginer/resqueueclinic/domain/model/User.java
@@ -4,27 +4,19 @@ package br.com.imaginer.resqueueclinic.domain.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
 @Table(schema = "user")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class User {
+public record User (
 
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
-        private UUID id;
+        UUID id,
 
         @Email
         @NotBlank(message = "O e-mail é obrigatório")
-        private String email;
+        String email
 
-}
+) {}
