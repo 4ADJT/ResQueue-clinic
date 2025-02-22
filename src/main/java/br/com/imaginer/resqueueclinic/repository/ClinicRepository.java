beb0d5090ca3b4a08aac5325c99ee03b8c61818c
rony1533 <ronysobral5@gmail.com>
2025-02-22 12:24:55 -0300

    feat: adicionando query para buscar pelo id do usuario e da clinica

--- src/main/java/br/com/imaginer/resqueueclinic/repository/ClinicRepository.java
+++ src/main/java/br/com/imaginer/resqueueclinic/repository/ClinicRepository.java
@@ -4,6 +4,13 @@ import br.com.imaginer.resqueueclinic.domain.model.Clinic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface ClinicRepository extends JpaRepository<Clinic, Long> {
+    List<Clinic> findByUserId(UUID userId);
+
+    Optional<Clinic> findByIdAndUserId(Long id, UUID userId);
 }
