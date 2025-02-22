8a42022d9c46e8e185af6115c1f650c6cc086876
rony1533 <ronysobral5@gmail.com>
2025-02-22 12:33:19 -0300

    refactor: utilizando o service apneas para o redirecionamento do chamado para um metodo

--- src/main/java/br/com/imaginer/resqueueclinic/service/ClinicService.java
+++ src/main/java/br/com/imaginer/resqueueclinic/service/ClinicService.java
@@ -1,8 +1,12 @@
 package br.com.imaginer.resqueueclinic.service;
 
+import br.com.imaginer.resqueueclinic.domain.form.ClinicForm;
 import br.com.imaginer.resqueueclinic.domain.model.Clinic;
+import br.com.imaginer.resqueueclinic.domain.model.User;
 import br.com.imaginer.resqueueclinic.repository.ClinicRepository;
+import br.com.imaginer.resqueueclinic.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,22 +16,52 @@ import java.util.UUID;
 public class ClinicService {
 
     private final ClinicRepository clinicRepository;
+    private final UserRepository userRepository;
 
-    public ClinicService(ClinicRepository clinicRepository) {
+    public ClinicService(ClinicRepository clinicRepository, UserRepository userRepository) {
         this.clinicRepository = clinicRepository;
+        this.userRepository = userRepository;
+    }
+
+    public List<Clinic> findAllByUserId(UUID userId) {
+        return clinicRepository.findByUserId(userId);
     }
 
     public Optional<Clinic> findByIdAndUserId(Long clinicId, UUID userId) {
         return clinicRepository.findByIdAndUserId(clinicId, userId);
     }
 
-    public Clinic save(Clinic clinic) {
-        return clinicRepository.save(clinic);
+    @Transactional
+    public Clinic createClinic(ClinicForm clinicForm) {
+        Optional<User> existingUser = userRepository.findById(clinicForm.getUser().id());
+
+        if (existingUser.isPresent()) {
+            throw new IllegalArgumentException("Esse Usuário já tem uma Clínica cadastrada em seu nome.");
+        }
+
+        User savedUser = userRepository.save(clinicForm.getUser());
+        Clinic newClinic = new Clinic(clinicForm.getName(), clinicForm.getAddress(), clinicForm.getPhone(), savedUser);
+
+        return clinicRepository.save(newClinic);
     }
 
-    public void deleteById(Long clinicId) {
-        clinicRepository.findById(clinicId).ifPresent(clinicRepository::delete);
+    @Transactional
+    public Optional<Clinic> updateClinic(Long clinicId, UUID userId, ClinicForm updatedClinic) {
+        return clinicRepository.findByIdAndUserId(clinicId, userId).map(existingClinic -> {
+            existingClinic.setName(updatedClinic.getName());
+            existingClinic.setAddress(updatedClinic.getAddress());
+            existingClinic.setPhone(updatedClinic.getPhone());
+            return clinicRepository.save(existingClinic);
+        });
     }
 
-    public List<Clinic> findAllByUserId(UUID userId) { return clinicRepository.findByUserId(userId); };
+    @Transactional
+    public boolean deleteClinic(Long clinicId, UUID userId) {
+        Optional<Clinic> clinic = clinicRepository.findByIdAndUserId(clinicId, userId);
+        if (clinic.isPresent()) {
+            clinicRepository.delete(clinic.get());
+            return true;
+        }
+        return false;
+    }
 }
