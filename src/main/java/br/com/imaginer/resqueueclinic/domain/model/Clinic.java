8840142381edc42a5424757962cc92bd39dd1dc4
rony1533 <ronysobral5@gmail.com>
2025-02-22 12:26:06 -0300

    refactor: alterando tipo do model para uma class

--- src/main/java/br/com/imaginer/resqueueclinic/domain/model/Clinic.java
+++ src/main/java/br/com/imaginer/resqueueclinic/domain/model/Clinic.java
@@ -1,29 +1,46 @@
 package br.com.imaginer.resqueueclinic.domain.model;
 
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "clinics")
+public class Clinic {
 
-public record Clinic (
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        Long id,
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-        @NotBlank(message = "O nome da clínica é obrigatório")
-        String name,
+    @NotBlank(message = "O nome da clínica é obrigatório")
+    private String name;
 
-        @NotBlank(message = "O endereço é obrigatório")
-        String addres,
+    @NotBlank(message = "O endereço é obrigatório")
+    private String address;
 
-        @Column(nullable = false)
-        String phone
-) {
+    @Column(nullable = false)
+    private String phone;
 
-    public Clinic(String name, String address, String phone) {
-        this(null, name, address, phone);
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Clinic() {}
+
+    public Clinic(String name, String address, String phone, User user) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.user = user;
     }
 
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public String getAddress() { return address; }
+    public String getPhone() { return phone; }
+    public User getUser() { return user; }
+
+    public void setName(String name) { this.name = name; }
+    public void setAddress(String address) { this.address = address; }
+    public void setPhone(String phone) { this.phone = phone; }
+    public void setUser(User user) { this.user = user; }
 }
