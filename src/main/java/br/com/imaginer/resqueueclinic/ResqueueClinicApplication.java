46bb0225b836c78580d75190779c78d73a14fa83
rodrigobrocchi <rodrigo.brocchi@gmail.com>
2025-02-24 04:12:04 -0300

    Enable Feign clients and add Authorization header for API requests.

--- src/main/java/br/com/imaginer/resqueueclinic/ResqueueClinicApplication.java
+++ src/main/java/br/com/imaginer/resqueueclinic/ResqueueClinicApplication.java
@@ -2,10 +2,9 @@ package br.com.imaginer.resqueueclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ResqueueClinicApplication {
 
